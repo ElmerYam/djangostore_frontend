62e06148e1482fc66d70a36f38af29d7a3525c41
ElmerYamRamirez <elmeryam5@gmail.com>
2021-08-30 16:47:47 -0500

    -base template modifications

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,14 @@
     <nav class="navbar is-dark">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Logo</strong></router-link>
-      </div>
-        
-      <a href="" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+      <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
+    </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/" class="navbar-item"><strong>item 2</strong></router-link>
           <router-link to="/" class="navbar-item"><strong>item 3</strong></router-link>
@@ -38,6 +37,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return{
+        showMobileMenu: false,
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
